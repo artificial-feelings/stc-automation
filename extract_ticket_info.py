9336cd51053296a28bf220ce97fafcb194eb57b8
kirill.kopot <kirill.kopot@libertexgroup.com>
2024-07-16 18:06:41 +0200

    write price as int

--- extract_ticket_info.py
+++ extract_ticket_info.py
@@ -190,10 +190,10 @@ def extract_ticket_info_layout():
                 df = read_google_sheet(sheet_service, google_sheet_id)
                 df = pd.concat([df, new_df], ignore_index=True)
                 if 'Price' in df.columns:
-                    df['Price'] = df['Price'].apply(lambda x: np.nan if (x is None) or (x == "") else x)
+                    #df['Price'] = df['Price'].apply(lambda x: np.nan if (x is None) or (x == "") else x)
                     df['Price'] = df['Price'].fillna(0).astype(int)
                 update_google_sheet(sheet_service, google_sheet_id, df)
-                
+
                 st.write("Обработка билетов завершена")
                 
                 # Clear the list of uploaded files
