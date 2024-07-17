5241deecf9d4974d37d4fa04c978cbf75becdbbb
kirill.kopot <kirill.kopot@libertexgroup.com>
2024-07-17 09:28:02 +0200

    write price as float

--- extract_ticket_info.py
+++ extract_ticket_info.py
@@ -190,7 +190,7 @@ def extract_ticket_info_layout():
                 df = read_google_sheet(sheet_service, google_sheet_id)
                 df = pd.concat([df, new_df], ignore_index=True)
                 if 'Price' in df.columns:
-                     df['Price'] = df['Price'].apply(lambda x: np.nan if (x is None) or (x == "") else x)
+                     df['Price'] = df['Price'].apply(lambda x: 0 if (x is None) or (x == "") else x)
                      df['Price'] = df['Price'].astype(float)
                 update_google_sheet(sheet_service, google_sheet_id, df)
 
